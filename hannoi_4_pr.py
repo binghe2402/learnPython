4ceeb83f9465fc35ccb5704f8a52f8109ec3e12c
binghe2402 <binghe2402@hotmail.com>
2020-04-01 00:02:08 +0800

    2

--- hannoi_4_pr.py
+++ hannoi_4_pr.py
@@ -3,12 +3,14 @@ def hannoi_4_num_move(n, h):
     if h[n] is not None:
         return h[n]
     elif n > 2:
-        h[n] = min([2*hannoi_4_num_move(n-x, h)+2**x-1 for x in range(1,n+1)])
+        # x不可能取到n，否则就是把这一堆整体移动，没有意义
+        h[n] = min([2*hannoi_4_num_move(n-x, h)+2**x-1 for x in range(1, n)])
         return h[n]
 
 
-n = int(input())
+# n = int(input())
+n = 10
 p = [2**x-1 for x in range(n+1)]
 h = [None]*(n+1)
-nn = hannoi_4_num_move(n,h)
+nn = hannoi_4_num_move(n, h)
 print(nn)
