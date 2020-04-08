030baa533342b1b3c2d4679c845703576c81026b
binghe2402 <binghe2402@hotmail.com>
2020-04-08 19:36:03 +0800

    整理和注释

--- y17.py
+++ y17.py
@@ -1,13 +1,16 @@
 class Tree(object):
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.cate = "plant"
-    def __getattribute__(self,*args,**kwargs):
+
+    def __getattribute__(self, *args, **kwargs):
         if args[0] == "大树":
             print("log 大树")
             return "我爱大树"
         else:
-            return object.__getattribute__(self,*args,**kwargs)
+            return object.__getattribute__(self, *args, **kwargs)
+
+
 aa = Tree("大树")
 print(aa.大树)
 print(aa.cate)
