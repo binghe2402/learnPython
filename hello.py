1e6f074e85a75aa4bbcdeb837c5534116eb1cd88
binghe2402 <binghe2402@hotmail.com>
2020-04-10 18:03:52 +0800

    整理

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
 import random
 
+
 def generate(len=4):
     string = '01234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
-    return random.choices(string,k=len)
- 
+    return random.choices(string, k=len)
 
 
 def main():
     len = int(input('请输入长度：'))
     code = generate(len)
-    print("".join(code))    
+    print("".join(code))
 
 
 if __name__ == '__main__':
