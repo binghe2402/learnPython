f017b714a1b28bd6996616cd78be08d7b186e615
binghe2402 <binghe2402@hotmail.com>
2020-04-21 14:10:03 +0800

    p976三角形最大周长

---
+++ Leetcode/p976_Largest_Perimeter_Triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestPerimeter(self, A: List[int]) -> int:
+        A.sort(reverse=1)
+        i, c = 0, 0
+        for i in range(len(A)-2):
+            if A[0+i] < A[1+i]+A[2+i]:
+                c = sum(A[0+i:3+i])
+                break
+        return c
