ff3ca7341e3a53cd2a6a1d8f0d97324c95e5b03d
binghe2402 <binghe2402@hotmail.com>
2020-05-04 02:29:22 +0800

    先移动i还是j的说明

--- 算法学习/排序/快速排序.py
+++ 算法学习/排序/快速排序.py
@@ -7,6 +7,18 @@ def quick_sort(nums: List):
         i, j = left, right
         if left < right:
             mid_val = nums[left]
+
+            # 先移动 i 还是先移动 j 的问题。
+            # 如果最后i j 停止时重合，那就先移动j，如现在的版本。即 i、j每次移动时，停止条件为 i < j
+            # 移动基准数（mid_val)时，交换i和基准数。不过由于i j 重合，所以实际交换j也可以。
+            # 递归分解时，用i分段。
+            #
+            # 如果i、j每次移动时，停止条件为 i <= j。那么最后停止时，i在j的右边。
+            # 这时就要先移动 i。移动基准数时，必须交换 j 和基准数。
+            # 因为要求交换后基准值左侧都比基准值小，而nums[j]才满足这个要求。
+            # 后面递归分解，也是用基准值的坐标，也就是 j 来分段。
+            # nums[i]\[j]与mid_val的判断中，必须带着等号（其实主要是nums[i])，
+            # 否则第一次交换nums[i]时就会是nums[left]
             while i < j:
                 while i < j and nums[j] >= mid_val:
                     j -= 1
@@ -25,3 +37,10 @@ nums = list(randint(1, 100, 500))
 res = quick_sort(nums)
 print(res)
 print(sorted(nums) == res)
+
+
+0 1
+1 2
+key = 1
+
+2 > 1
