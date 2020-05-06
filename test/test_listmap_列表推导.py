eecd70e96d1d7a97383e983319e2f2ec2cb49aa3
binghe2402 <binghe2402@hotmail.com>
2020-05-07 03:45:09 +0800

    更多测试

--- test/test_listmap_列表推导.py
+++ test/test_listmap_列表推导.py
@@ -1,4 +1,4 @@
-''' 如果需要运算 直接循环求最小值比列表推导然后min()更快 '''
+''' 不一定谁快。前一个map再list快，后一个列表推导快 '''
 
 import timeit
 
@@ -11,5 +11,20 @@ s2 = 'b = [int(i) for i in s]'
 
 t1 = timeit.timeit(stmt=s1, setup=ss, number=2000000)
 t2 = timeit.timeit(stmt=s2, setup=ss, number=2000000)
+print(t1)
+print(t2)
+
+
+ss = '''
+with open('e://project_data//file_selector//random_list_b0000.txt', 'r') as random_list:
+    select_lst = random_list.readlines()
+'''
+
+s1 = "a = list(map(lambda x:x.split(),select_lst))"
+s2 = 'b = [i.split() for i in select_lst]'
+
+
+t1 = timeit.timeit(stmt=s1, setup=ss, number=200)
+t2 = timeit.timeit(stmt=s2, setup=ss, number=200)
 print(t1)
 print(t2)
