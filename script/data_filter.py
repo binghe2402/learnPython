bcdde8c96dbd7025351937a2e4a3db92637bf27d
binghe2402 <binghe2402@hotmail.com>
2020-05-08 00:29:53 +0800

    修改random_list为对应组态 修改示例 修改输出文件

--- script/data_filter.py
+++ script/data_filter.py
@@ -6,7 +6,7 @@
     筛选path_data下的数据文件。
 
     读取每一个数据文件，提取文件名中的 _t0_x0_y0_z0 坐标，
-    筛选 random_list 文件中 t 坐标为 t0 + delta_t 的行，t x y z。
+    筛选对应组态的 random_list 文件中 t 坐标为 t0 + delta_t 的行，t x y z。
     二者相减得到相对坐标，  t-t0 = delta_t, x-x0, y-y0, z-z0
     所有坐标值 t∈[0,48). x,y,z∈[0,24)
     取出数据文件中，坐标值为相对坐标的行，
@@ -42,36 +42,46 @@ range_xyz = 24
 
 path_data = r''
 path_filter = r'./test'
-path_list = r'e:/project_data/file_selector/random_list_b0000.txt'
-out_file = r'output.txt'
+path_list = r'e:/project_data/file_selector/'
+out_path = r''
+out_file_name_pre = 'pip_position_u_block_'
+file_ext = '.txt'
+# coordinate
+coord_pos = [19, 22, 25, 28]
+coord_len = 2
+# configuration
+conf_pos = 13
+conf_len = 5
 
-num_pos = [18, 21, 24, 27]
-num_len = 2
-
-file_filter = os.walk(path_filter)
-
-# 储存所有random_list的数据，
-# 结构为以 t 为索引的 t 个list，
-# 每个list中为这个t对应的所有(x,y,z)元组
-with open(path_list, 'r') as random_list:
-    select_lst = [[] for i in range(range_t)]
-    for line in random_list.readlines():
-        t, x, y, z = map(int, line.split())
-        select_lst[t].append((x, y, z))
+file_filter = os.listdir(path_filter)
 
 
-for roots, dirs, files in file_filter:
-    print(roots)
-    print(files)
-    print('='*15)
-    for file_name in files:
-        print(file_name)
+def get_random_list(path_list, conf, pre='random_list_'):
+    # 储存所有random_list的数据，
+    # 结构为以 t 为索引的 t 个list，
+    # 每个list中为这个t对应的所有(x,y,z)元组
+    file_name = pre+conf
+    with open(os.path.join(path_list, file_name), 'r') as random_list:
+        select_lst = [[] for i in range(range_t)]
+        for line in random_list.readlines():
+            t, x, y, z = map(int, line.split())
+            select_lst[t].append((x, y, z))
+    return select_lst
+
+
+def main():
+    for file_name in file_filter:
         file_name = file_name.replace('random_pp', 'position')
         print(file_name)
 
+        # 提取组态
+        conf = file_name[conf_pos:conf_pos+conf_len]
+
+        select_lst = get_random_list(path_list, conf)
+
         # 提取文件名的坐标值
         # manual
-        t, x, y, z = (int(file_name[i:i+num_len]) for i in num_pos)
+        t, x, y, z = (int(file_name[i:i+coord_len]) for i in coord_pos)
         # auto
         # t, x, y, z = [int(i) for i in re.findall(r'(?<=_)\d{2}', file_name)]
 
@@ -83,7 +93,7 @@ for roots, dirs, files in file_filter:
                            for x_l, y_l, z_l in select_lst[(t+delta_t) % range_t]]
         data = {}
         # 读取数据文件
-        with open(path_data + file_name, 'r') as data_file:
+        with open(os.path.join(path_data, file_name), 'r') as data_file:
             # data_line = True
             # while data_line:
             #     data_line = data_file.readline()
@@ -98,6 +108,10 @@ for roots, dirs, files in file_filter:
                     data[pos] = data_line
 
         # 按照顺序追加写入到输出文件
-        with open(out_file, 'a') as out:
+        with open(os.path.join(out_path, out_file_name_pre+conf+file_ext), 'a') as out:
             for pos in select_lst_rela:
                 out.write(data[pos]+'\n')
+
+
+if __name__ == "__main":
+    main()
