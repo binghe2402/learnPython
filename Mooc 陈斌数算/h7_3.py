8e93660ecb6e27719435bbe1348a5f195f8fe492
binghe2402 <binghe2402@hotmail.com>
2020-05-10 02:06:43 +0800

    作业

--- Mooc 陈斌数算/h7_3.py
+++ Mooc 陈斌数算/h7_3.py
@@ -1,57 +1,73 @@
-import random
-# nums = list(map(int, input().split()))
-# nums_liter = list(map(int, input().split()))
+# import random
+nums = list(map(int, input().split()))
+nums_liter = list(map(int, input().split()))
 
 merge_liter = []
 insert_liter = []
 
 
 def merge_sort(nums):
-    def divide(nums):
-        if len(nums) == 1:
-            return nums
-        else:
-            mid = len(nums)//2
-            nums1 = divide(nums[:mid])
-            nums2 = divide(nums[mid:])
-            return merge(nums1, nums2)
+    nums = nums[:]
 
     def merge(nums1, nums2):
-        i = j = 0
         nums = []
+        i = j = 0
         while i < len(nums1) and j < len(nums2):
-            if nums1[i] > nums2[j]:
-                nums.append(nums2[j])
-                j += 1
-            else:
+            if nums1[i] < nums2[j]:
                 nums.append(nums1[i])
                 i += 1
+            else:
+                nums.append(nums2[j])
+                j += 1
         if i < len(nums1):
             nums.extend(nums1[i:])
-        else:
+        elif j < len(nums2):
             nums.extend(nums2[j:])
-        # merge_liter.append(nums[:])
         return nums
-    return divide(nums)
+
+    gap = 1
+    while gap <= len(nums):
+        gap *= 2
+        for i in range(0, len(nums), gap):
+            nums_cell = nums[i:i+gap]
+            mid = gap//2
+            nums1 = nums_cell[:mid]
+            nums2 = nums_cell[mid:]
+            if nums2:
+                nums[i:i+gap] = merge(nums1, nums2)
+        yield nums
+    # return nums
 
 
 def insert_sort(nums):
+    nums = nums[:]
     for i in range(1, len(nums)):
         temp = nums[i]
-        for j in range(1, i):
-            if temp < nums[i-j]:
-                nums[i-j+1] = nums[i-j]
-            else:
-                nums[i-j+1] = temp
-        # yield nums
-    return nums
+        j = 1
+        # for j in range(1, i+1):
+        while j <= i and temp < nums[i-j]:
+            # if temp < nums[i-j]:
+            nums[i-j+1] = nums[i-j]
+            j += 1
+        else:
+            nums[i-j+1] = temp
+            # break
+        yield nums
+    # return nums
 
 
-nums = [random.randint(1, 200) for i in range(1000)]
+# nums = [random.randint(1, 200) for i in range(1000)]
 true_sort = sorted(nums)
-merge = merge_sort(nums)
-insert = insert_sort(nums)
-print(merge_liter)
-print(insert)
-print("merge_sort:%r" % (merge == true_sort))
-print("insert_sort:%r" % (insert[-1] == true_sort))
+merge_liter = merge_sort(nums)
+insert_liter = insert_sort(nums)
+for seq in merge_liter:
+    if seq == nums_liter:
+        print('Merge Sort')
+        print(('%d '*len(nums)).strip() % tuple(i for i in next(merge_liter)))
+        break
+
+for seq in insert_liter:
+    if seq == nums_liter:
+        print('Insertion Sort')
+        print(('%d '*len(nums)).strip() % tuple(i for i in next(insert_liter)))
+        break
